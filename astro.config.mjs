54e43b92228d9df4a7361bcb3da010be84edef78
Dominik <dominik.witschard@gmx.ch>
2025-09-07 21:35:40 +0200

    Adjusted deployed config URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dwitschard.github.io/breadly/',
 	integrations: [mdx(), sitemap()],
 	base: '.',
 });
