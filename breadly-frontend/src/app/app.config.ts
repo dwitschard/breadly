22c0917f696397bf2645051e9164ae02765e1fcb
Dominik Witschard <V009403@vontobel.com>
2026-03-09 20:15:27 +0100

    implemented openapi spec in frontend

--- breadly-frontend/src/app/app.config.ts
+++ breadly-frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideApi } from './generated/api/provide-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApi('http://localhost:3000'),
+  ],
 };
--- breadly-frontend/src/app/app.routes.ts
+++ breadly-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'recipe',
+    loadComponent: () =>
+      import('./features/recipes/recipes.component').then((m) => m.RecipesComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'recipe',
+    pathMatch: 'full',
+  },
+];
