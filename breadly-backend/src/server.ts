06a86079b0905d540cb56160c146172490037600
Dominik Witschard <V009403@vontobel.com>
2026-03-18 11:10:56 +0100

    Re-enabled DB Connection

--- breadly-backend/src/server.ts
+++ breadly-backend/src/server.ts
@@ -1,15 +1,15 @@
 import {app} from "./app.js";
+import 'dotenv/config'
+import {ApplicationDatabase} from "./database/application-database.js";
+import {MONGODB_CONFIG} from "./database/mongodb.config.js";
 
-// dotenv.config();
 const PORT = process.env['PORT'] ?? 3000;
 
 const startServer = async () => {
-    /*
       await ApplicationDatabase.init(
         MONGODB_CONFIG.CONNECTION_STRING(),
         MONGODB_CONFIG.DB_NAME,
       );
-      */
 
     app.listen(PORT, () => {
         console.log('Server is running on port 3000');
